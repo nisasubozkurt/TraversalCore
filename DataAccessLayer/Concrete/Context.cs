215c10c15bf44df38656e05b6ba2d8586949ec10
Nisasu Bozkurt <bozkurtnisasu@hotmail.com>
2023-07-04 19:05:16 +0300

    ilk commit

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-TO0C5CS2;database=TraversalDB;integrated security=true;");
+        }
+
+        public DbSet<About> Abouts { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<About2> Abouts2 { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Contact> Contacts { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Destination> Destinations { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Feature> Features { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Feature2> Features2 { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Guide> Guides { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Newsletter> Newsletters { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<SubAbout> SubAbouts { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+        public DbSet<Testimonial> Testimonials { get; set; } // s takılı hali tablo ismi olsun, entity ile karışmasın
+
+
+    }
+}
