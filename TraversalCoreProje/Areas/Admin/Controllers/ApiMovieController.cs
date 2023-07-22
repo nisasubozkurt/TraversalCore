0b5ac58e6e9798b85fe7b359357b5421b2825de5
Nisasu Bozkurt <bozkurtnisasu@hotmail.com>
2023-07-22 13:26:11 +0300

    71.ders sonu Rapid Api Eklendi

---
+++ TraversalCoreProje/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using TraversalCoreProje.Areas.Admin.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "b0d06eeef2msh84d24b3e6412085p12d286jsnbea64ded0371" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies=JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+        }
+    }
+}
