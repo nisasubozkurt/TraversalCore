3661fb26107ab798965eb26ea535990f7bc0ae59
Nisasu Bozkurt <bozkurtnisasu@hotmail.com>
2023-07-24 19:00:46 +0300

    95.ders sonu revize yapıldı

---
+++ TraversalCoreProje/ViewComponents/MemberLayout/_MemberLayoutHead.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutHead : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
